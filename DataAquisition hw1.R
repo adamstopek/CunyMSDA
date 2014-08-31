332a3cff9a457a70246876d2bcc205a872e41a7c
Adam Stopek <adamstopek@gmail.com>
2014-08-31 21:42:59 +0300

    diff 1

--- DataAquisition hw1.R
+++ DataAquisition hw1.R
@@ -2,7 +2,7 @@ install.packages("DMwR")
 library(DMwR)
 data(sales, package='DMwR')
 nrow(sales)
-
+# ans: 401146
 # R version:  3.1.0
 # RStudio Version: 0.98.953
-# PostgreSQL Version:
+# PostgreSQL Version:9.3
