48942150820b0d859e1ded34be9294cca83b6cb5
Adam Stopek <adamstopek@gmail.com>
2014-09-26 14:02:56 +0300

    Added a bit more notation

--- Assignment_week_5.R
+++ Assignment_week_5.R
@@ -18,6 +18,10 @@ messy_data <- data.frame (
     "young_yes" = c(80100, 99400),
     "young_no" = c(35900, 43000)  
    )
+messy_data
+#city old_yes old_no young_yes young_no
+#1 edinburgh  143000 214800     80100    35900
+#2   glasgow  150400 207000     99400    43000
 
 ##### question 3 #####
 #convert to tidy data
@@ -33,14 +37,46 @@ messy_data %>%
 gather(age_decision, votes, -city ) %>%
 separate (age_decision, c("age", "decision"), "_")
 )
+tidy_data
+#city   age decision  votes
+#1 edinburgh   old      yes 143000
+#2   glasgow   old      yes 150400
+#3 edinburgh   old       no 214800
+#4   glasgow   old       no 207000
+#5 edinburgh young      yes  80100
+#6   glasgow young      yes  99400
+#7 edinburgh young       no  35900
+#8   glasgow young       no  43000
+
+
 
 ##### question 4 #####
 #Answer the questions from q1 using plyr
 
 require (plyr)
 by_age <- ddply(tidy_data, .(age, decision), summarize, total = sum(votes))
+by_age
+#age decision  total
+#1   old       no 421800
+#2   old      yes 293400
+#3 young       no  78900
+#4 young      yes 179500
+
 by_city <- ddply(tidy_data, .(city, decision), summarize, total = sum(votes))
+by_city
+
+#city decision  total
+#1 edinburgh       no 250700
+#2 edinburgh      yes 223100
+#3   glasgow       no 250000
+#4   glasgow      yes 249800
+
 total_result <- ddply(tidy_data, .(decision), summarize, total = sum(votes))
+total_result
+
+#decision  total
+#1       no 500700
+#2      yes 472900
 
 ##### question 5 #####
 # When I started the assignment, I had the end goal in mind.
