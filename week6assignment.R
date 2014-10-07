65556d0a57582b06df9c08eaa92421f648535dd1
Adam Stopek <adamstopek@gmail.com>
2014-10-07 09:55:30 +0300

    update 2

--- week6assignment.R
+++ week6assignment.R
@@ -8,17 +8,41 @@
 #install_github("hadley/rvest")
 #library(rvest)
 
-url = "http://office.microsoft.com/en-us/buy-microsoft-office-2013-suites-and-office-365-subscriptions-FX102886268.aspx"
-x = html(url)
-list <- html_nodes(x, ".pmgTxtFs24 , .heading5")
-scraped_data <- html_text(list)
-df <- data.frame(scraped_data)
+google_news = html("https://news.google.com/")
+scraped <- html_nodes(google_news, ".esc-lead-article-title")
+Google_News_Headlines<- html_text(scraped)
 
-#removing unneccessary data
-ptn = "{.*?"
-ndx = grep(ptn, df$scraped_data, perl = T, invert=T)
-list_of_microsoft_office_products <- as.character(df[ndx,])
-list_of_microsoft_office_products
+Google_News_Headlines
+#ans
 
-# ans: [1] "Word"       "Excel"      "PowerPoint" "OneNote"    "Outlook"    "Publisher"  "Access" 
-
+#[1] "Battle rages for Syrian border town"                                                  
+#[2] "Ebola outbreak: Spanish nurse is first person to contract disease outside western ..."
+#[3] "NHS funding crisis: Boss warns of £75-a-night charge for a hospital bed"              
+#[4] "Twin Peaks: why there's never been anything like it"                                  
+#[5] "Obama Vow on Ebola Screening Means Plugging Airport Gaps"                             
+#[6] "Roy Keane lets rip at Sir Alex Ferguson in new book as he lifts lid on his stormy ..."
+#[7] "Shrien Dewani's millionaire lifestyle revealed in detail as murder trial begins"      
+#[8] "Mexico Disarms Police in Missing Students City"                                       
+#[9] "Putin: Muzhik To Ears Of Ordinary Russians"                                           
+#[10] "Teneriffe murder-suicide: Former friend describes Marcus Volke as paranoid, in ..."   
+#[11] "IAEA To Meet Iranian Officials In Tehran"                                             
+#[12] "Alice Gross murder: Man's body formally identified as prime suspect Arnis ..."        
+#[13] "Ukip: Nigel Farage's plan to win eight seats"                                         
+#[14] "Liberal Democrat conference: Swinson could be sole survivor of endangered ..."        
+#[15] "Rio Tinto rejects talk of Glencore merger"                                            
+#[16] "Libya's $1bn hangover – fund's losses after drinks with Goldman"                      
+#[17] "Samsung: New Phones Amid 60% Profit Drop"                                             
+#[18] "German industrial output plunges at fastest rate since 2009 financial crisis ..."     
+#[19] "iPhone 6 'prototype' bids reach $100000"                                              
+#[20] "AT&T fired employee who improperly accessed customer accounts"                        
+#[21] "Tracey Emin: The Last Great Adventure is You review – a lesson in how to be a ..."    
+#[22] "The Kitchen; Grantchester review – a domestic-espionage documentary and a ..."        
+#[23] "Simon Cowell Is All About That Bass - Watch Simon Cover The Meghan Trainor Hit"       
+#[24] "Pietersen: ECB used PR tricks"                                                        
+#[25] "FA would consider taking foreign route again when replacing Hodgson"                  
+#[26] "Bristol's latest green building opens today"                                          
+#[27] "UN biodiversity report highlights failure to meet conservation targets"               
+#[28] "Why this photograph isn't what it seems"                                              
+#[29] "Honeysuckle tea could fight flu"                                                      
+#[30] "Preschools Consider Requiring Flu Shots"                                              
+#[31] "Cannabis 'addictive and causes mental health problems', 20 year study finds"  
